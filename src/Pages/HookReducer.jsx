a36cc05e19cabb4dbdad0247d29c23dc6fb21740
WibyFabian08 <wibyfabian08@gmail.com>
2021-01-11 10:54:34 -0800

    Hooks useReducer Kompleks State

--- src/Pages/HookReducer.jsx
+++ src/Pages/HookReducer.jsx
@@ -1,15 +1,25 @@
 import React, { useReducer } from "react";
 import { Row, Col, Card, Button } from "react-bootstrap";
-import { CustomInput } from "reactstrap";
 
-const initialState = 0;
+const initialState = {
+    jumlah: 1,
+    hargaSatuan: 10000,
+    hargaTotal: 10000
+}
 
 const reducer = (state, action) => {
-    switch(action) {
-        case 'tambah': return state + 1
-        case 'kurang': return state - 1
+    switch(action. type) {
+        case 'tambah': return {
+            ...state,
+            jumlah: state.jumlah + 1,
+            hargaTotal: state.hargaSatuan + (state.jumlah * state.hargaSatuan)
+        }
+        case 'kurang': return {
+            ...state,
+            jumlah: state.jumlah - 1,
+            hargaTotal: (state.jumlah * state.hargaSatuan) - state.hargaSatuan
+        }
         default: return state
-
     }
 }
 
@@ -19,12 +29,12 @@ const HooksReducer = () => {
     const [count, dispatch] = useReducer(reducer, initialState);
     
     function tambah() {
-        dispatch('tambah');
+        dispatch({type: 'tambah'});
     }
 
     function kurang() {
-        if(count >= 1) {
-            dispatch('kurang');
+        if(count.jumlah >= 1) {
+            dispatch({type: 'kurang'});
         }
     }
 
@@ -41,19 +51,20 @@ const HooksReducer = () => {
         <Col>
           <h3>Scanner</h3>
           <p>Harga</p>
-          <p>Rp. 150.000</p>
+          <p>Rp. {count.hargaSatuan}</p>
           <p>Jumlah</p>
           <Row clasName="d-flex justify-content-center">
             <Col>
               <Button variant="danger" onClick={() => kurang()}> - </Button>
             </Col>
             <Col>
-              <p>{count}</p>
+              <p>{count.jumlah}</p>
             </Col>
             <Col>
               <Button variant="primary" onClick={() => tambah()}> + </Button>
             </Col>
           </Row>
+          <Button className='mt-3' variant='success'>Total Harga Rp. {count.hargaTotal}</Button>
         </Col>
       </Row>
     </div>
